49349e95bd53f28b82f9e3100f243c12f7cc3bc5
DanRHenry <danielrhenry@gmail.com>
2024-12-29 20:12:01 -0500

    added to own repo

---
+++ index.js
@@ -0,0 +1,21 @@
+let inputFieldForP1Name = document.getElementById("inputFieldForP1Name");
+let inputFieldForP2Name = document.getElementById("inputFieldForP2Name");
+let submitNameButton = document.getElementById("submitNameButton");
+localStorage.setItem("scrabblePlayerOneName", "");
+localStorage.setItem("scrabblePlayerTwoName", "");
+
+submitNameButton.addEventListener("click", () => {
+        let player1Name = inputFieldForP1Name.value;
+        let player2Name = inputFieldForP2Name.value;
+          // console.log(player1Name);
+          // console.log(player2Name);
+        localStorage.setItem("scrabblePlayerOneName", player1Name);
+        localStorage.setItem("scrabblePlayerTwoName", player2Name);
+        location.href = "./gameBoard.html";
+});
+
+// function submitName(yourNameHere) {
+//   saveName();
+//   location.href = "./gameBoard.html";
+// }
+
